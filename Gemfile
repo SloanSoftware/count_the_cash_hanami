4183640a8677d85b82cdd68090f90a748cfc4cb2
John Sloan <jwsloan@gmail.com>
2017-10-03 22:41:57 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+gem 'hanami', '~> 1.0'
+gem 'hanami-model', '~> 1.0'
+gem 'rake'
+
+gem 'sqlite3'
+
+group :development do
+  # Code reloading
+  # See: http://hanamirb.org/guides/projects/code-reloading
+  gem 'shotgun'
+end
+
+group :test, :development do
+  gem 'dotenv', '~> 2.0'
+  gem 'rubocop'
+end
+
+group :test do
+  gem 'capybara'
+  gem 'minitest'
+end
+
+group :production do
+  # gem 'puma'
+end
---
+++ config/environment.rb
@@ -0,0 +1,49 @@
+require 'bundler/setup'
+require 'hanami/setup'
+require 'hanami/model'
+require_relative '../lib/count_the_cash'
+require_relative '../apps/web/application'
+
+Hanami.configure do
+  mount Web::Application, at: '/'
+
+  model do
+    ##
+    # Database adapter
+    #
+    # Available options:
+    #
+    #  * SQL adapter
+    #    adapter :sql, 'sqlite://db/count_the_cash_development.sqlite3'
+    #    adapter :sql, 'postgresql://localhost/count_the_cash_development'
+    #    adapter :sql, 'mysql://localhost/count_the_cash_development'
+    #
+    adapter :sql, ENV['DATABASE_URL']
+
+    ##
+    # Migrations
+    #
+    migrations 'db/migrations'
+    schema     'db/schema.sql'
+  end
+
+  mailer do
+    root 'lib/count_the_cash/mailers'
+
+    # See http://hanamirb.org/guides/mailers/delivery
+    delivery :test
+  end
+
+  environment :development do
+    # See: http://hanamirb.org/guides/projects/logging
+    logger level: :debug
+  end
+
+  environment :production do
+    logger level: :info, formatter: :json
+
+    mailer do
+      delivery :smtp, address: ENV['SMTP_HOST'], port: ENV['SMTP_PORT']
+    end
+  end
+end
